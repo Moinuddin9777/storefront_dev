4789d8203df0a8deb691e10855d3fc38cfa1c73a
Thanoj <thanuj2932@gmail.com>
2024-03-06 12:40:37 +0530

    Sorting

--- mobile/storefront/lib/products/widgets/products_list.dart
+++ mobile/storefront/lib/products/widgets/products_list.dart
@@ -245,13 +245,13 @@ class _ProductsListState extends State<ProductsListView> {
                       Text(
                         'Rating: ${typeFilteredList[index]['rating']}',
                         style: const TextStyle(
-                          fontSize: 11.5,
+                          fontSize: 11,
                         ),
                       ),
                       Text(
                         'Price \$${typeFilteredList[index]['price']}',
                         style: const TextStyle(
-                          fontSize: 11.5,
+                          fontSize: 11,
                         ),
                       ),
                     ],
