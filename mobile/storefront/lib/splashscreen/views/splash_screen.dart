87ee20dd75d6d41ac37598dc3be9280e0687e459
Hemanth Pawan <155997082+hemanthpawan345@users.noreply.github.com>
2024-03-06 16:07:41 +0530

    spalshscreen sign in up screen erroes cleared

--- mobile/storefront/lib/splashscreen/views/splash_screen.dart
+++ mobile/storefront/lib/splashscreen/views/splash_screen.dart
@@ -1,38 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:storefront/auth/controller/controller.dart';
-import 'package:storefront/auth/view/signin_screen.dart';
-import 'package:storefront/bottom_bar/view/home.dart';
+import 'package:storefront/splashscreen/controller/controller.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  changeScreen() {
-    Future.delayed(const Duration(seconds: 3), () {
-      FirebaseAuth.instance.authStateChanges().listen((User? user) {
-        if (user == null && mounted) {
-          Get.offAll(() => const SigninScreen());
-        } else {
-          Get.offAll(() => const Home());
-        }
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    Get.put(AuthController());
-    super.initState();
-    changeScreen();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    Get.put(SplashScreenController());
+    Get.put(AuthController());
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
